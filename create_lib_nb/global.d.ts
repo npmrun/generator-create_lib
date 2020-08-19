fcf7dbffae435285adaafa6c09c1d4103eb1954a
npmrun <kk1549469775@gmail.com>
2020-08-19 13:01:09 +0800

    文档更新

--- create_lib_nb/global.d.ts
+++
@@ -1,4 +0,0 @@
-
-
-// 全局替换
-declare var __DEV__: boolean;
--- create_lib_nb/scripts/dev.js
+++
@@ -1,7 +0,0 @@
-const execa = require("execa");
-const chalk = require('chalk');
-
-console.log(chalk.red("开始监听,格式:umd"));
-execa("rollup", ["-wc", "--environment", `NODE_ENV:development,TARGET:umd`], {
-  stdio: "inherit",
-});
