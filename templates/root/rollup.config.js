91af3f94a277b429de1178d8fb9e00968fea5152
npmrun <kk1549469775@gmail.com>
2021-07-01 00:24:09 +0800

    修复Bug

--- templates/root/rollup.config.js
+++ templates/root/rollup.config.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 let isProd = process.env.NODE_ENV === "production";
 
-const input = "src/index.ts";
+const input = "source/index.ts";
 const openSourceMap = true;
 const buildOptions = pkg.buildOptions;
 buildOptions.formats = process.env.TARGET
@@ -19,9 +19,17 @@ buildOptions.formats = process.env.TARGET
 
 export default createConfig();
 
+let external = [];
+
+external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
 function createConfig() {
   return {
     input,
+    external,
     output: createOutput(buildOptions),
     plugins: createPlugin(),
   };
@@ -52,17 +60,26 @@ function createPlugin() {
 
 function createOutput(options) {
   const output = [];
+  const globals = {
+
+  };
   const filterType = {
     //可用script标签导入，会输出一个全局变量
     iife: {
       sourcemap: openSourceMap,
       file: `dist/${options.filename}.iife.js`,
       format: "iife",
+      globals,
+      exports: "auto",
+      externalLiveBindings: false,
       name: options.var,
     },
     // amd规范，可用require.js加载
     amd: {
       sourcemap: openSourceMap,
+      exports: "auto",
+      globals,
+      externalLiveBindings: false,
       file: `dist/${options.filename}.amd.js`,
       format: "amd",
     },
@@ -71,24 +88,35 @@ function createOutput(options) {
       sourcemap: openSourceMap,
       file: `dist/${options.filename}.umd.js`,
       format: "umd",
+      globals,
       name: options.var,
+      exports: "auto",
+      externalLiveBindings: false,
     },
     // CommonJS规范
     cjs: {
       sourcemap: openSourceMap,
       file: `dist/${options.filename}.cjs.js`,
       format: "cjs",
+      globals,
       exports: "auto",
+      externalLiveBindings: false,
       banner: "#!/usr/bin/env node" // 提供命令行的可执行权限
     },
     // ES2015 Module 规范, 可用 `Webpack`, `Rollup` 加载
     esm: {
       sourcemap: openSourceMap,
+      exports: "auto",
+      externalLiveBindings: false,
+      globals,
       file: `dist/${options.filename}.esm.js`,
       format: "es",
     },
     "esm-browser": {
       sourcemap: openSourceMap,
+      exports: "auto",
+      globals,
+      externalLiveBindings: false,
       file: `dist/${options.filename}.esm-browser.js`,
       format: "es",
     },
